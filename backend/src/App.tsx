6c6b31e0cb17a371718906a6630f67030cf2bf9c
Jakeline Gomes <jakeline5793@gmail.com>
2026-04-18 16:43:36 -0300

    Remove frontend duplicado do backend

--- backend/src/App.tsx
+++
@@ -1,8 +0,0 @@
-import './styles/App.css'
-import Home from './pages/Home'
-
-function App() {
-  return <Home />
-}
-
-export default App
--- backend/src/pages/Home.tsx
+++
@@ -1,7 +0,0 @@
-import Chat from '../components/Chat'
-
-const Home = () => {
-    return <Chat />
-}
-
-export default Home
